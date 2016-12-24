93f82f40e37b5ea05945a42a2369d4c3a2a5efc0
Haojie <etafate@gmail.com>
2016-12-25 00:14:32 +0800

    Add EnterSinglePlayersNameActivity

--- app/src/main/java/com/haojie/badmintonscorecounter/MainActivity.java
+++ app/src/main/java/com/haojie/badmintonscorecounter/MainActivity.java
@@ -2,12 +2,30 @@ package com.haojie.badmintonscorecounter;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button mStartSinglesButton;
+    private Button mStartDoublesButton;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mStartSinglesButton = (Button)findViewById(R.id.button_singles);
+        mStartDoublesButton = (Button)findViewById(R.id.button_doubles);
+
+        mStartSinglesButton.setOnClickListener(new View.OnClickListener()
+        {
+           @Override
+            public void onClick(View v)
+           {
+
+           }
+        });
     }
 }
