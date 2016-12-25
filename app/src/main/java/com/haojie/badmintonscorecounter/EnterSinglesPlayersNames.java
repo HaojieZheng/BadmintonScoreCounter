23f4ddd26be6be63a19f270a503c395cd6a0ba1b
Haojie <etafate@gmail.com>
2016-12-25 13:19:24 +0800

    Add swap button capabilites

--- app/src/main/java/com/haojie/badmintonscorecounter/EnterSinglesPlayersNames.java
+++ app/src/main/java/com/haojie/badmintonscorecounter/EnterSinglesPlayersNames.java
@@ -2,12 +2,33 @@ package com.haojie.badmintonscorecounter;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.TextView;
 
 public class EnterSinglesPlayersNames extends AppCompatActivity {
+
+    ImageButton mSwapButton;
+    EditText mEditPlayer1Name;
+    EditText mEditPlayer2Name;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_enter_singles_players_names);
+
+        mSwapButton = (ImageButton)findViewById(R.id.swap_button);
+        mEditPlayer1Name = (EditText)findViewById(R.id.editPlayer1Name);
+        mEditPlayer2Name = (EditText)findViewById(R.id.editPlayer2Name);
+
+        mSwapButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String temp = mEditPlayer1Name.getText().toString();
+                mEditPlayer1Name.setText(mEditPlayer2Name.getText().toString(), TextView.BufferType.EDITABLE);
+                mEditPlayer2Name.setText(temp, TextView.BufferType.EDITABLE);
+            }
+        });
     }
 }
