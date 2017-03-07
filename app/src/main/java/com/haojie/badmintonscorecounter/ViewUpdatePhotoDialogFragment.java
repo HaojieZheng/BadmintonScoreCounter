92078ab239dd1cf461ce12135aa3f9a0b0c67ba3
Haojie <etafate@gmail.com>
2017-03-07 23:55:59 +0800

    Fix some static analysis warnings

--- app/src/main/java/com/haojie/badmintonscorecounter/ViewUpdatePhotoDialogFragment.java
+++ app/src/main/java/com/haojie/badmintonscorecounter/ViewUpdatePhotoDialogFragment.java
@@ -9,7 +9,6 @@ import android.provider.MediaStore;
 import android.support.v4.app.DialogFragment;
 import android.content.Context;
 import android.os.Bundle;
-import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
