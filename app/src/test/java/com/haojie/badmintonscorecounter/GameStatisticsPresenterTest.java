323c1c85847cc5e92983885c38bbf1ffd6d09287
Haojie <etafate@gmail.com>
2017-03-13 20:32:34 +0800

    Refactoring of test

--- app/src/test/java/com/haojie/badmintonscorecounter/GameStatisticsPresenterTest.java
+++ app/src/test/java/com/haojie/badmintonscorecounter/GameStatisticsPresenterTest.java
@@ -15,6 +15,16 @@ import static org.mockito.Mockito.when;
  */
 @RunWith(MockitoJUnitRunner.class)
 public class GameStatisticsPresenterTest {
+
+    @Mock
+    private IDatabase mockDatabase;
+
+    @Mock
+    private Game mockGame;
+
+    private Player mPlayer1 = new Player("Test1");
+    private Player mPlayer2 = new Player("Test2");
+
     @Test
     public void getTopNPlayers_empty_database(){
 
@@ -32,12 +42,10 @@ public class GameStatisticsPresenterTest {
     public void getTopNPlayers_get_zero(){
 
         Database database = new Database();
-        Player p1 = new Player("Test1");
-        Player p2 = new Player("Test2");
-        database.addPlayer(p1);
-        database.addPlayer(p2);
+        database.addPlayer(mPlayer1);
+        database.addPlayer(mPlayer2);
 
-        database.addGame(createGameWithWinner(p1, p2, 1));
+        database.addGame(createGameWithWinner(mPlayer1, mPlayer2, 1));
 
         GameStatisticsPresenter presenter = new GameStatisticsPresenter(database);
 
@@ -48,24 +56,21 @@ public class GameStatisticsPresenterTest {
         assertTrue(result.isEmpty());
     }
 
-    @Mock
-    IDatabase mockDatabase;
 
     @Test
     public void getTopNPlayers_get_two_but_only_one_win(){
-        Player p1 = new Player("Test1");
-        Player p2 = new Player("Test2");
+
 
         ArrayList<Player> players = new ArrayList<>();
-        players.add(p1);
-        players.add(p2);
+        players.add(mPlayer1);
+        players.add(mPlayer2);
         when(mockDatabase.getPlayersWithoutDefault()).thenReturn(players);
 
         ArrayList<Game> games = new ArrayList<>();
-        games.add(createGameWithWinner(p1, p2, 1));
+        games.add(createGameWithWinner(mPlayer1, mPlayer2, 1));
         when(mockDatabase.getGames()).thenReturn(games);
-        when(mockDatabase.getPlayerWithName("Test1")).thenReturn(p1);
-        when(mockDatabase.getPlayerWithName("Test2")).thenReturn(p2);
+        when(mockDatabase.getPlayerWithName("Test1")).thenReturn(mPlayer1);
+        when(mockDatabase.getPlayerWithName("Test2")).thenReturn(mPlayer2);
 
         GameStatisticsPresenter presenter = new GameStatisticsPresenter(mockDatabase);
 
@@ -73,7 +78,7 @@ public class GameStatisticsPresenterTest {
 
         ArrayList<GameStatisticsPresenter.PlayerWinEntry> result = presenter.getTopNPlayers(2);
         assertEquals(1, result.size());
-        assertEquals(p1, result.get(0).getPlayer());
+        assertEquals(mPlayer1, result.get(0).getPlayer());
         assertEquals(1, result.get(0).getWins());
     }
 
@@ -82,14 +87,12 @@ public class GameStatisticsPresenterTest {
     public void getTopNPlayers_get_three_but_only_two(){
 
         Database database = new Database();
-        Player p1 = new Player("Test1");
-        Player p2 = new Player("Test2");
-        database.addPlayer(p1);
-        database.addPlayer(p2);
+        database.addPlayer(mPlayer1);
+        database.addPlayer(mPlayer2);
 
-        database.addGame(createGameWithWinner(p1, p2, 1));
-        database.addGame(createGameWithWinner(p1, p2, 1));
-        database.addGame(createGameWithWinner(p1, p2, 2));
+        database.addGame(createGameWithWinner(mPlayer1, mPlayer2, 1));
+        database.addGame(createGameWithWinner(mPlayer1, mPlayer2, 1));
+        database.addGame(createGameWithWinner(mPlayer1, mPlayer2, 2));
 
         GameStatisticsPresenter presenter = new GameStatisticsPresenter(database);
 
@@ -97,9 +100,9 @@ public class GameStatisticsPresenterTest {
 
         ArrayList<GameStatisticsPresenter.PlayerWinEntry> result = presenter.getTopNPlayers(2);
         assertEquals(2, result.size());
-        assertEquals(p1, result.get(0).getPlayer());
+        assertEquals(mPlayer1, result.get(0).getPlayer());
         assertEquals(2, result.get(0).getWins());
-        assertEquals(p2, result.get(1).getPlayer());
+        assertEquals(mPlayer2, result.get(1).getPlayer());
         assertEquals(1, result.get(1).getWins());
     }
 
